0580a73da9a067a3937ab1a4bc6d6e666ff90389
amkillia <amk184@case.edu>
2015-04-29 13:57:10 -0400

    added comments to .cpp file, renamed cpp file

---
+++ catkin/src/cwru_376_student/ashley_ros_rockwell/src/ros_client.cpp
@@ -0,0 +1,131 @@
+#define _USE_MATH_DEFINES
+
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define MAX_FLOAT_LEN 32
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+// make sine wave a method if desired
+/*float motorMove(long double omega) {
+    double a = 1.0;
+    double t = 0.0;
+    double dt = 1.0;
+    double x_des = a*sin(omega * t);
+    t+=dt;
+    return x_des;
+}*/
+
+int main(int argc, char **argv) {
+
+    // name of this node will be "vel_scheduler"
+    ros::init(argc, argv, "vel_scheduler"); 
+
+    // get a ros nodehandle; standard
+    ros::NodeHandle nh; 
+
+    // create a publisher object that can talk to ROS and issue twist messages on named topic;
+    // note: this is customized for stdr robot; 
+    ros::Publisher pub  = nh.advertise<std_msgs::Float32>("joint_angle_command", 1);
+    
+    // x = a*sin(omega*t)
+    long double omega = 3.14;
+    double a = 1.0;
+    double t = 0.0;
+
+    // incrementing t 
+    double dt = 0.1; 
+
+    //allow callbacks to populate fresh data
+    ros::spinOnce(); 
+
+
+    int sockfd, portno, n;
+
+    // name of server is it's IP Address
+    char *name = "192.168.20.5";
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    // creating string ASCII
+    char floatArray[MAX_FLOAT_LEN];
+
+    // Create socket for client
+    ROS_INFO("about to create socket");
+    portno = 10002;  // port number of server
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);  // standard
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(name);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    // Name the socket as agreed with server
+    memset((char *) &serv_addr,0, sizeof(serv_addr)); // sets bytes in memory
+    serv_addr.sin_family = AF_INET; // select internet protocol
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,  // address
+         server->h_length);
+    serv_addr.sin_port = htons(portno); // select port number
+
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted (or ctl-C)
+    while (ros::ok()) 
+    { 
+        // publishing x = a*sin(omega * t)
+        std_msgs::Float32 ros_float;
+        double x_des = a*sin(omega * t);
+        t+=dt;
+        ros_float.data = x_des;
+        pub.publish(ros_float);
+        
+        // read and write
+        ROS_INFO("about to read and write");
+
+        // setting bytes in memory for string ASCII
+        memset(floatArray, 0, sizeof(floatArray));  
+        snprintf(floatArray,sizeof(floatArray),"%f",ros_float.data);
+        ROS_INFO("floatArray %s", floatArray);
+
+        // send string ASCII to server, n = 1 if successful
+        n = send(sockfd,floatArray, strlen(floatArray), 0);
+        if (n < 0)
+             error("ERROR writing to socket");
+
+        ROS_INFO("move sent to controller");
+
+        // here, enable client to receive data from server
+        /*n = read(sockfd,floatArray,255);
+        if (n < 0)
+             error("ERROR reading from socket");
+        printf("%s\n",floatArray);*/
+
+        ros::Duration(0.1).sleep();
+
+    }  
+    // done with reading and writing to controller
+    ROS_INFO("completed move distance");
+
+    // Close client socket
+    close(sockfd);
+
+    return 0;
+}
+
